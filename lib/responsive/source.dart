a1baaa72151ecdd97c6693887e2e494694e16298
kazaliika <mavero811@gmail.com>
2024-05-10 22:23:44 +0700

    tambah gambar

--- lib/responsive/source.dart
+++ lib/responsive/source.dart
@@ -19,5 +19,12 @@ var kumpulanwisataList = [
     deskripsi:
         'Masjid Agung As-Salam terletak di jantung Kota Lubuklinggau Provinsi Sumatera Selatan. Masjid ini tak hanya sekadar menjadi tempat ibadah bagi umat Islam namun telah menjadi salah satu objek wisata religi di provinsi Sumatra Selatan. Masjid ini bukan hanya sebagai tempat ibadah yang menjadi kebanggan masyarakat Kota Lubuk Linggau tetapi juga menjadi icon Kota Lubuk Linggau.',
     imageAsset: 'assets/masjid_agung.jpg',
-  )
+  ),
+  KumpulanWisata(
+    nama: 'Taman Olahraga Megang',
+    lokasi: 'Lubuk Linggau',
+    deskripsi:
+        'Taman Olahraga Megang merupakan sebuah sport center yang berada di Kota Lubuklinggau. Pada masa pimpinan Wali Kota Lubuklinggau, Bapak H. S.N Prana Putra Sohe, bangunan yang semula bernama Gedung Olahraga (GOR Megang) ini kemudian direnovasi sehingga memenuhi Peraturan Daerah Provinsi Sumatera Selatan Nomor 2 tahun 2021 tentang Arsitektur Bangunan Gedung Berornamen Khas Budaya Sumatera Selatan.',
+    imageAsset: 'assets/tom.jpg',
+  ),
 ];
